fe6b4dbe3930556a1364b05b60e03dbb393a9f69
Syed Sumaim Ali <syedsumaimaly08@gmail.com>
2023-08-18 14:35:23 +0100

    Third Commit

---
+++ Xtra-WP-Technology-Clone/index.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll(".count");
+const speed = 200;
+
+console.log("count" ,counters)
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = parseInt(+counter.getAttribute("data-target"));
+    const count = parseInt(+counter.innerText);
+    const increment = Math.trunc(target / speed);
+    console.log(increment);
+
+    if (count < target) {
+      counter.innerText = count + increment;
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+  updateCount();
+});
